b6650877d628266195b2e4b75254522cf9e74144
Tema Klochko <tema@klochko.ru>
2021-02-06 16:29:30 +0700

    Simplify the picking mechanism

--- lg_webos_cli/__main__.py
+++ lg_webos_cli/__main__.py
@@ -23,25 +23,25 @@ controls_subsystems = {
 if __name__ == '__main__':
     addr = ConnectionCache.read()
     if not addr:
-        clients = WebOSClient.discover()
-        clients_indices = range(len(clients) + 1, 1)
+        clients = {str(index): client for index, client
+                   in enumerate(WebOSClient.discover(), 1)}
         entered = None
-        while entered is None or entered not in clients_indices:
+        while entered is None or entered not in clients.keys():
             entered = input('\n'.join([
-                *[') '.join([str(i), c.host]) for i, c
-                  in zip(clients_indices, clients)],
+                *[') '.join([i, c.host]) for i, c in clients.items()],
                 '(q to quit)\n',
             ]))
             if entered == 'q':
                 exit()
-        client = clients[entered]
+        client = clients.get(entered)
+        addr = client.host
     else:
         client = WebOSClient(addr)
     client.connect()
     store = CredStorage.load(addr)
     for status in client.register(store):
         if status == WebOSClient.PROMPTED:
-            print('Please accept the connection on the TV!')
+            print('Please accept the connection on the TV')
             ConnectionCache.write(client)
         if status == WebOSClient.REGISTERED:
             CredStorage.persist(addr, store)

5197baa81b15c11a0e8f4288f87c294e8b3d0b56
Tema Klochko <tema@klochko.ru>
2021-03-02 20:04:40 +0700

    Remove controls from __main__ Those are now available via caller.controls

--- lg_webos_cli/__main__.py
+++ lg_webos_cli/__main__.py
@@ -4,19 +4,12 @@ from logging import getLogger
 from sys import stdout
 
 from pywebostv.connection import WebOSClient
-from pywebostv.controls import WebOSControlBase
 
 from lg_webos_cli.caller import call, controls
 from lg_webos_cli.connection_cache import ConnectionCache
 from lg_webos_cli.store import CredStorage
 
 logger = getLogger(__name__)
-
-controls_subsystems = {
-    e: subsystem
-    for subsystem in WebOSControlBase.__subclasses__()
-    for e in subsystem.COMMANDS
-}
 
 if __name__ == '__main__':
     parser = ArgumentParser(description='LG WebOS CLI wrapper')

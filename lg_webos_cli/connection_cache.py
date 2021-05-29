3702a10de42e56ea43b55782a84835cdb9b3059d
Tema Klochko <tema@klochko.ru>
2021-05-29 18:16:35 +0700

    Use json as a connection cache storage backend

--- lg_webos_cli/connection_cache.py
+++ lg_webos_cli/connection_cache.py
@@ -1,14 +1,15 @@
+from json import dumps, loads
 from os import path
 from typing import Optional
 
-from pywebostv.connection import WebOSClient
 from getmac import get_mac_address
+from pywebostv.connection import WebOSClient
 
 
 class ConnectionCache:
 
-    KEY_IP_ADDR = 0
-    KEY_MAC_ADDR = 1
+    KEY_IP_ADDR = 'ip_addr'
+    KEY_MAC_ADDR = 'mac_addr'
     TEMP_FILE_PATH = path.join(
         path.dirname(path.realpath(__file__)),
         '..',
@@ -18,10 +19,10 @@ class ConnectionCache:
     @classmethod
     def write(cls, client: WebOSClient):
         with open(cls.TEMP_FILE_PATH, 'w') as cache:
-            cache.write('\n'.join([
-                client.host,
-                get_mac_address(client.host),
-            ]))
+            cache.write(dumps({
+                cls.KEY_IP_ADDR: client.host,
+                cls.KEY_MAC_ADDR: get_mac_address(client.host),
+            }))
 
     @classmethod
     def read_addr(cls) -> Optional[str]:
@@ -35,6 +36,6 @@ class ConnectionCache:
     def _read(cls, key: int) -> Optional[str]:
         try:
             with open(cls.TEMP_FILE_PATH, 'r') as cache:
-                return cache.readlines()[key].rstrip('\n') or None
+                return loads(cache.read()).get(key) or None
         except FileNotFoundError:
             return None
